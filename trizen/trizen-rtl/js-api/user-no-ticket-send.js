d51c5e7a13816b00c997b4ef716495dad5ee709e
Nima Fadavi <fadavinima1@gmail.com>
2025-03-05 03:15:11 +0330

    final project files

--- trizen/trizen-rtl/js-api/user-no-ticket-send.js
+++
@@ -1,7 +0,0 @@
-var $ = document;
-let sendTicket = $.getElementById('sendTicket');
-
-sendTicket.addEventListener('click', () => {
-    dashboardAreaNoTicket.classList.add('d-none')
-    dashboardAreaSendTicket.classList.remove('d-none')
-});
